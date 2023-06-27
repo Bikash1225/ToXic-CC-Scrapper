44f402c53649ebc9e221a022e05eedf836573c2e
Real Hopper <113882185+givemehope07@users.noreply.github.com>
2023-06-28 01:19:00 +0530

    Update button.py

--- button.py
+++ button.py
@@ -1,11 +1,14 @@
-from pyrogram.types import InlineKeyboardButton, Message
+from pyrogram import Client
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 KID = "1057412250"
 
 
-def add_button(message: Message):
-    button = InlineKeyboardButton(text="🥀 ᴅᴇᴠᴇʟᴏᴩᴇʀ 🥀",url=f"tg://user?id={KID}")
+def handle_message(client, message):
+    button = InlineKeyboardButton(text="🥀 ᴅᴇᴠᴇʟᴏᴩᴇʀ 🥀", url=f"tg://user?id={KID}")
+
     if message.reply_markup:
-        message.reply_markup.inline_keyboard.append([button])
+        message.edit_reply_markup(reply_markup=message.reply_markup.inline_keyboard + [[button]])
     else:
-        message.reply_markup = InlineKeyboardMarkup([[button]])
+        message.edit_reply_markup(reply_markup=InlineKeyboardMarkup([[button]]))
+
